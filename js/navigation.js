5c88eaf94887f87f4ca9252ed55faf852d4f8f91
doublekunzite <supporttheresistance@proton.me>
2025-12-03 16:37:55 -0600

    convert use cases to blog

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,7 @@
 // Navigation structure - edit this ONE file to update all pages
 const navItems = [
     { name: 'Home', url: 'index.html' },
-    { name: 'Explore Uses', url: 'use-cases.html' },
+    { name: 'Blog', url: 'blog.html' },
     { name: 'Prompts', url: 'prompts.html' },
     { name: 'Prompt Builder', url: 'prompt-builder.html' },
     { name: 'Learn the Risks', url: 'risks.html' },
@@ -11,10 +11,10 @@ const navItems = [
 
 function injectNavigation() {
     const currentPage = window.location.pathname.split('/').pop() || 'index.html';
-    
+
     // Inject site title BEFORE nav
     const titleHTML = `<h1 class="site-title" style="text-align:center; margin: 1em 0 0.5em; font-size: 2.5em;">What Can AI Do?</h1>`;
-    
+
     const navHTML = `
         <nav>
             <ul>
@@ -24,7 +24,7 @@ function injectNavigation() {
             </ul>
         </nav>
     `;
-    
+
     let navContainer = document.getElementById('nav-container');
     if (!navContainer) {
         navContainer = document.createElement('div');
@@ -32,7 +32,7 @@ function injectNavigation() {
         const main = document.querySelector('main');
         if (main) main.prepend(navContainer);
     }
-    
+
     navContainer.innerHTML = titleHTML + navHTML;
 }
 
